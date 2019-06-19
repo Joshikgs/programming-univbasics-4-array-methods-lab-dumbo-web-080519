c8486909c0d9e54f79d8102a25c46b7bd56cdf68
Josh Nho <joshnho@gmail.com>
2019-06-19 23:07:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
-  array.include?(element1)
-  array.include?(element2)
+  return array.include?(element1)
+  return array.include?(element2)
 end
 
 def using_sort(array)
